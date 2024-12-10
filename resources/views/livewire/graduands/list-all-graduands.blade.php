4e2b1a4a81205494dc0a86a4246aea957f74d2fc
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2024-12-10 13:28:37 +0300

    block href loading

--- resources/views/livewire/graduands/list-all-graduands.blade.php
+++ resources/views/livewire/graduands/list-all-graduands.blade.php
@@ -23,7 +23,7 @@
                         <a href="#"
                             class="inline-flex items-center px-3 py-1.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
                             friend</a>
-                        <a href="" wire:prevent wire:click="createChat({{ $graduand->id }})"
+                        <a href="javascript:void(0)" wire:click.prevent="createChat({{ $graduand->id }})"
                             class="py-2 px-3 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Message</a>
                     </div>
                 </div>
