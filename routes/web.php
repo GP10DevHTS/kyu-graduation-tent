c18f79578dbdd1960b3958e6cdfa2598fbf8910c
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2024-12-10 13:28:18 +0300

    block dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Graduands\ListAllGraduands;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +13,10 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Route::get('/dashboard', function () {
+    //     return redirect()->route('graduands.list');
+    // })->name('dashboard');
+    Route::redirect('/dashboard', 'graduands')->name('dashboard');
 
     Route::get('/graduands', ListAllGraduands::class)->name('graduands.list');
 });
